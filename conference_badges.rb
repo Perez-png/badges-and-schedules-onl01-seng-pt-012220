679b7b349de552be929cda0c284423b696987c28
Xavier Perez <xperez007@gmail.com>
2020-01-27 20:28:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
+badge_maker("Arel")
+=> "Hello, my name is Arel."
+badge_maker("Edsger")
+=> "Hello, my name is Edsger."
+badge_maker("Ada")
+=> "Hello, my name is Ada."
+badge_maker("Charles")
+=> "Hello, my name is Charles."
+badge_maker("Alan")
+=> "Hello, my name is Alan."
+=> badge_maker("Grace")
+=> "Hello, my name is Grace."
+badge_maker("Linus)
+=> "Hello, my name is Linus."
+badge_maker( Matz)
+=> "Hello, my name is Matz."
 # Write your code here.
